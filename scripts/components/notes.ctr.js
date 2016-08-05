2ac77ba06838d6f230cc14a4d592f54ac57b00df
itsshady101 <rocks.ayush2011@gmail.com>
2016-08-05 19:07:32 +0530

    Note Creation Done

--- scripts/components/notes.ctr.js
+++ scripts/components/notes.ctr.js
@@ -3,9 +3,31 @@
 
 	angular
 		.module("ngNotes")
-		.controller("notesCtrl", function($scope, $http, notesFactory) {
+		.controller("notesCtrl", function($scope, $http, notesFactory, $mdSidenav, $mdToast) {
 			notesFactory.getNotes().then(function(data){
 				$scope.notes = data.data;
 			});
+
+			$scope.openSidebar = function() {
+				$mdSidenav("left").open();
+			}
+
+			$scope.closeSidebar = function() {
+				$mdSidenav("left").close();
+			}
+
+			$scope.saveNote = function(note) {
+				if (note) {
+					note.posted = new Date();
+					$scope.notes.push(note);
+					$scope.closeSidebar();
+					$mdToast.show(
+						$mdToast.simple()
+							.content("Note Created")
+							.position('top, right')
+							.hideDelay(3000)
+					);
+				}
+			}
 		});
 })();
