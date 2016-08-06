cb64e26774815c5cd4e7226fbe91a2ed2d61804a
itsshady101 <rocks.ayush2011@gmail.com>
2016-08-06 18:47:49 +0530

    Note edit, deleting and filtering completed

--- scripts/components/notes.ctr.js
+++ scripts/components/notes.ctr.js
@@ -3,10 +3,13 @@
 
 	angular
 		.module("ngNotes")
-		.controller("notesCtrl", function($scope, $http, notesFactory, $mdSidenav, $mdToast) {
+		.controller("notesCtrl", function($scope, $http, notesFactory, $mdSidenav, $mdToast, $mdDialog, $mdSelect) {
 			notesFactory.getNotes().then(function(data){
 				$scope.notes = data.data;
+				$scope.categories = getCategories($scope.notes);
+
 			});
+
 
 			$scope.openSidebar = function() {
 				$mdSidenav("left").open();
@@ -21,13 +24,57 @@
 					note.posted = new Date();
 					$scope.notes.push(note);
 					$scope.closeSidebar();
-					$mdToast.show(
-						$mdToast.simple()
-							.content("Note Created")
-							.position('top, right')
-							.hideDelay(3000)
-					);
+					showToast("Note Saved", 3000);
 				}
 			}
+
+			$scope.editNote = function(note) {
+				$scope.editing = true;
+				$scope.openSidebar();
+				$scope.note = note;
+			}
+
+			$scope.saveEdit = function() {
+				$scope.editing = false;
+				$scope.note = {};
+				$scope.closeSidebar();
+				showToast("Note Edited", 3000);
+			}
+
+			$scope.deleteNote = function(event,note) {
+				var confirm = $mdDialog.confirm()
+								.title('You are about to delete ' + note.title + '?')
+								.ok('Yes')
+								.cancel('No')
+								.targetEvent(event);
+
+				$mdDialog.show(confirm).then(function(){
+					var index = $scope.notes.indexOf(note);
+					$scope.notes.splice(index,1);
+				},function(){
+					
+				});
+
+			}
+
+			function showToast($content, $delay) {
+				$mdToast.show(
+					$mdToast.simple()
+						.content($content)
+						.position('top, right')
+						.hideDelay($delay)
+				);
+			}
+
+			function getCategories(notes) {
+				var categories = [];
+				angular.forEach(notes, function(item){
+					angular.forEach(item.category, function(category){
+						categories.push(category);	
+					});
+				});
+				return _.uniq(categories);
+			}
+
 		});
 })();
